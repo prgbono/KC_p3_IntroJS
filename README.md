1c362e3fc8a5c2c4e274e7a1295e49f7ca7aa4be
Francisco Ríos <pacoriosgalan@gmail.com>
2020-12-20 19:39:19 +0100

    Modifying ReadMe and package description. Modify teams. Add exercise pdf

--- README.md
+++ README.md
@@ -1,18 +1,4 @@
-# Introdución a JS
+# KC BootCamp Web X. Introdución a JS
+# Fco Ríos.
 
-En este curso aprendermos a trabajar con JavaScript como lenguaje de programación. 
-
-Aunque usaremos node, no entraremos en los detalles de cómo funciona JavaScript en Node o en un navegador, nos centraremos únicamente en usar y aprender el lenguaje. 
-
-Para aprender, desarrollaremos un **simulador de ligas de fútbol**. 
-
-Será un programa que realizará una programación de los partidos en las diferentes jornadas y llevará el conteo de estadísticas en base a los resultados de los partidos simulados. 
-
-El funcionamiento deseado será el siguiente:
-
-- Mostrar los equipos inscritos por pantalla.
-- Crear la planificación de jornadas y partidos de cada jornada.
-- Mostrarla por pantalla.
-- Jugar lo partidos de todas las jornadas.
-- Una vez terminada cada jornada, se deberá mostrar cómo queda la clasificación de la misma.
-- Una vez terminada la liga, se mostrarán estádísticas de número de goles totales y total de puntos ganados.
+Este repositorio contiene la práctica de este módulo a partir de un fork del código realizado y explicado en clase por [Alberto Casero](https://github.com/kasappeal).
--- teams.js
+++ teams.js
@@ -1,8 +1,28 @@
-export const liverPoolTeams = ['Liverpool', 'Everton']
-export const manchesterTeams = ['Manchester City', 'Manchester United']
-export const londonTeams = ['Arsenal', 'Chelsea', 'Fulham', 'West Ham', 'Tottenham', 'Crystal Palace']
-export const premierLeagueTeams = [
-    ...liverPoolTeams,
-    ...manchesterTeams,
-    ...londonTeams
-]
+export const groupA = ['Sudáfrica', 'México', 'Uruguay', 'Francia'];
+export const groupB = ['Argentina', 'Corea del Sur', 'Nigeria', 'Grecia'];
+export const groupC = ['Inglaterra', 'Estados Unidos', 'Argelia', 'Eslovenia'];
+export const groupD = ['Alemania', 'Australia', 'Ghana', 'Serbia'];
+export const groupE = ['Holanda', 'Japón', 'Camerún', 'Dinamarca'];
+export const groupF = ['Italia', 'Nueva Zelanda', 'Paraguay', 'Eslovaquia'];
+export const groupG = ['Brasil', 'Corea del Norte', 'Costa de Marfil', 'Portugal'];
+export const groupH = ['España', 'Honduras', 'Chile', 'Suiza'];
+  
+
+// export const liverPoolTeams = ['Liverpool', 'Everton']
+// export const manchesterTeams = ['Manchester City', 'Manchester United']
+// export const londonTeams = ['Arsenal', 'Chelsea', 'Fulham', 'West Ham', 'Tottenham', 'Crystal Palace']
+// export const premierLeagueTeams = [
+//     ...liverPoolTeams,
+//     ...manchesterTeams,
+//     ...londonTeams
+// ]
+
+
+// export const europeTeams = [
+//     'España', 'Francia', 'Alemania', 'Inglaterra', 'Grecia', 'Eslovenia', 'Serbia', 'Holanda', 'Dinamarca', 'Italia', 'Eslovaquia', 'Portugal', 'Suiza'];
+// export const americaTeams = [
+//     'Estados Unidos', 'México', 'Uruguay', 'Argentina', 'Paraguay', 'Brasil', 'Honduras', 'Chile'];
+// export const asiaTeams = ['Corea del Sur', 'Japón', 'Corea del Norte'];
+// export const africaTeams = ['Sudáfrica', 'Nigeria', 'Argelia', 'Ghana', 'Camerún', 'Costa de Marfil'];
+// export const oceaniaTeams = ['Australia', 'Nueva Zelanda'];
+
